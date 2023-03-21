cbe83ea71d8a7306f56518f350df11bf014dc2d9
MSreeN <khyathimnsree369@gmail.com>
2023-03-21 19:20:09 +0530

    added comments explaining event delegation

--- script.js
+++ script.js
@@ -79,6 +79,9 @@ btnScrollTo.addEventListener('click', function (e) {
 //     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
 //   });
 // });
+//Instead of adding click function to every nav like we did above, we make use of event delegation where we will attach that click function to parent element which is common to all navigation anchor tags like ".nav__links".
+// so when we click on the any of the nav element it will cause click event and that event will bubble up to the top of the document and in the way we have parent element of nav__link which is "nav__links" click event function will execute in the bubbling phase.
+// We were able to navigate to the respective section when clicked on the respective nav__link by using e.target, this will give the target element on which click event is generated and which caused the click event function in the parent element nav__links to execute during the bubbling phase and we are getting the section id which is hard coded in the html and set the "scrollIntoView" on that particular e.target element. 
 
 document.querySelector('.nav__links').addEventListener('click',e => {
   e.preventDefault();
