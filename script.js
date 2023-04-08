bd5b6c1fe1f39dbe30760cade95e3541a7250ebd
MSreeN <khyathimnsree369@gmail.com>
2023-04-08 20:27:47 +0530

    implemented dots funcitonality

--- script.js
+++ script.js
@@ -334,6 +334,7 @@ const activateDot = function(slide){
   const dots = document.querySelectorAll(".dots__dot");
   dots.forEach(dot => {
     dot.classList.remove("dots__dot--active")
+    document.querySelector(`.dots__dot[data-slide = "${slide}"]`).classList.add("dots__dot--active");
   })
 
 }
